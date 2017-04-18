e604182a13ecc19bf330fd16bc9f5ca78e01db71
Matt Cheney <matt@getpantheon.com>
2017-04-18 10:50:10 -0700

    Updates to Backstop and Visual Regression

--- bin/drupal-auto-update.sh
+++ bin/drupal-auto-update.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-MULTIDEV="update-dr"
-
 UPDATES_APPLIED=false
 
 # login to Terminus
@@ -9,57 +7,57 @@ echo -e "\nLogging into Terminus..."
 terminus auth:login --machine-token=${TERMINUS_MACHINE_TOKEN}
 
 # delete the multidev environment
-echo -e "\nDeleting the ${MULTIDEV} multidev environment..."
-terminus multidev:delete $SITE_UUID.$MULTIDEV --delete-branch --yes
+echo -e "\nDeleting the ${TERMINUS_ENV} multidev environment..."
+terminus multidev:delete $SITE_UUID.$TERMINUS_ENV --delete-branch --yes
 
 # recreate the multidev environment
-echo -e "\nRe-creating the ${MULTIDEV} multidev environment..."
-terminus multidev:create $SITE_UUID.live $MULTIDEV
+echo -e "\nRe-creating the ${TERMINUS_ENV} multidev environment..."
+terminus multidev:create $SITE_UUID.live $TERMINUS_ENV
 
 # check for upstream updates
-echo -e "\nChecking for upstream updates on the ${MULTIDEV} multidev..."
+echo -e "\nChecking for upstream updates on the ${TERMINUS_ENV} multidev..."
 # the output goes to stderr, not stdout
-UPSTREAM_UPDATES="$(terminus upstream:updates:list $SITE_UUID.$MULTIDEV  --format=list  2>&1)"
+UPSTREAM_UPDATES="$(terminus upstream:updates:list $SITE_UUID.$TERMINUS_ENV  --format=list  2>&1)"
 
 if [[ ${UPSTREAM_UPDATES} == *"No updates"* ]]
 then
     # no upstream updates available
-    echo -e "\nNo upstream updates found on the ${MULTIDEV} multidev..."
+    echo -e "\nNo upstream updates found on the ${TERMINUS_ENV} multidev..."
 else
     # making sure the multidev is in git mode
-    echo -e "\nSetting the ${MULTIDEV} multidev to git mode"
-    terminus connection:set $SITE_UUID.$MULTIDEV git
+    echo -e "\nSetting the ${TERMINUS_ENV} multidev to git mode"
+    terminus connection:set $SITE_UUID.$TERMINUS_ENV git
 
     # apply Drupal upstream updates
-    echo -e "\nApplying upstream updates on the ${MULTIDEV} multidev..."
-    terminus upstream:updates:apply $SITE_UUID.$MULTIDEV --yes --updatedb --accept-upstream
+    echo -e "\nApplying upstream updates on the ${TERMINUS_ENV} multidev..."
+    terminus upstream:updates:apply $SITE_UUID.$TERMINUS_ENV --yes --updatedb --accept-upstream
     UPDATES_APPLIED=true
 fi
 
 # making sure the multidev is in SFTP mode
-echo -e "\nSetting the ${MULTIDEV} multidev to SFTP mode"
-terminus connection:set $SITE_UUID.$MULTIDEV sftp
+echo -e "\nSetting the ${TERMINUS_ENV} multidev to SFTP mode"
+terminus connection:set $SITE_UUID.$TERMINUS_ENV sftp
 
 # check for Drupal module updates
-echo -e "\nChecking for Drupal module updates on the ${MULTIDEV} multidev..."
-PLUGIN_UPDATES="$(terminus drush $SITE_UUID.$MULTIDEV -- plugin list --update=available --format=count)"
+echo -e "\nChecking for Drupal module updates on the ${TERMINUS_ENV} multidev..."
+PLUGIN_UPDATES="$(terminus drush $SITE_UUID.$TERMINUS_ENV -- plugin list --update=available --format=count)"
 
 if [[ ${PLUGIN_UPDATES} == "0" ]]
 then
     # no Drupal module updates found
-    echo -e "\nNo Drupal module updates found on the ${MULTIDEV} multidev..."
+    echo -e "\nNo Drupal module updates found on the ${TERMINUS_ENV} multidev..."
 else
     # update Drupal modules
-    echo -e "\nUpdating Drupal modules on the ${MULTIDEV} multidev..."
-    terminus drush $SITE_UUID.$MULTIDEV -- pm-update drupal
+    echo -e "\nUpdating Drupal modules on the ${TERMINUS_ENV} multidev..."
+    terminus drush $SITE_UUID.$TERMINUS_ENV -- pm-update drupal
 
     # wake the site environment before committing code
-    echo -e "\nWaking the ${MULTIDEV} multidev..."
-    terminus env:wake -n $SITE_UUID.$MULTIDEV
+    echo -e "\nWaking the ${TERMINUS_ENV} multidev..."
+    terminus env:wake -n $SITE_UUID.$TERMINUS_ENV
 
     # committing updated Drupal modules
-    echo -e "\nCommitting Drupal modules updates on the ${MULTIDEV} multidev..."
-    terminus env:commit $SITE_UUID.$MULTIDEV --message="update Drupal modules" --yes
+    echo -e "\nCommitting Drupal modules updates on the ${TERMINUS_ENV} multidev..."
+    terminus env:commit $SITE_UUID.$TERMINUS_ENV --message="update Drupal modules" --yes
     UPDATES_APPLIED=true
    
     
@@ -80,40 +78,38 @@ else
     npm install
 
     # ping the multidev environment to wake it from sleep
-    echo -e "\nPinging the ${MULTIDEV} multidev environment to wake it from sleep..."
-    terminus env:wake -n $SITE_UUID.$MULTIDEV
+    echo -e "\nPinging the ${TERMINUS_ENV} multidev environment to wake it from sleep..."
+    terminus env:wake -n $SITE_UUID.$TERMINUS_ENV
 
     # backstop visual regression
     echo -e "\nRunning BackstopJS tests..."
-
-    cd node_modules/backstopjs
-
-    npm run reference
-    # npm run test
-
-    VISUAL_REGRESSION_RESULTS=$(npm run test)
+    backstop reference
+    VISUAL_REGRESSION_RESULTS=$(backstop test || echo 'true')
 
     echo "${VISUAL_REGRESSION_RESULTS}"
 
     cd -
     if [[ ${VISUAL_REGRESSION_RESULTS} == *"Mismatch errors found"* ]]
     then
-        # visual regression failed
-        echo -e "\nVisual regression tests failed! Please manually check the ${MULTIDEV} multidev..."
-        SLACK_MESSAGE="Circle CI update check #${CIRCLE_BUILD_NUM} by ${CIRCLE_PROJECT_USERNAME}. Visual regression tests failed on <https://dashboard.pantheon.io/sites/${SITE_UUID}#${MULTIDEV}/code|the ${MULTIDEV} environment>! Please test manually."
+        # Visual Regression Failed. Get Visual Difference Image
+        VISUAL_IMAGE_URL=$(php -f bin/post_image.php `find . | grep png | grep failed`)
+
+        echo -e "\nVisual regression tests failed! Please manually check the ${TERMINUS_ENV} multidev..."
+        SLACK_MESSAGE="Circle CI update check #${CIRCLE_BUILD_NUM} by ${CIRCLE_PROJECT_USERNAME}. Visual regression tests failed on <https://dashboard.pantheon.io/sites/${SITE_UUID}#${TERMINUS_ENV}/code|the ${TERMINUS_ENV} environment>! Please review and manually - ${VISUAL_IMAGE_URL}"
         echo -e "\nSending a message to the ${SLACK_CHANNEL} Slack channel"
-        curl -X POST --data "payload={\"channel\": \"${SLACK_CHANNEL}\", \"username\": \"${SLACK_USERNAME}\", \"text\": \"${SLACK_MESSAGE}\"}" $SLACK_HOOK_URL
+        SLACK_ICON: "http://live-drupalcon-github-magic.pantheonsite.io/sites/default/files/icons/backstop.png"
+        curl -X POST --data "payload={\"channel\": \"${SLACK_CHANNEL}\", \"icon_url\": \"${SLACK_ICON}\". \"username\": \"${SLACK_USERNAME}\", \"text\": \"${SLACK_MESSAGE}\"}" $SLACK_HOOK_URL
         exit 1
     else
         # visual regression passed
-        echo -e "\nVisual regression tests passed between the ${MULTIDEV} multidev and live."
+        echo -e "\nVisual regression tests passed between the ${TERMINUS_ENV} multidev and live."
 
         # enable git mode on dev
         echo -e "\nEnabling git mode on the dev environment..."
         terminus connection:set $SITE_UUID.dev git
 
         # merge the multidev back to dev
-        echo -e "\nMerging the ${MULTIDEV} multidev back into the dev environment (master)..."
+        echo -e "\nMerging the ${TERMINUS_ENV} multidev back into the dev environment (master)..."
         terminus connection:set $SITE_UUID.dev git
         
         # deploy to test
