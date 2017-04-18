1160919044091994ff6e9dae3c3a518209a12680
Matt Cheney <matt@getpantheon.com>
2017-04-18 10:21:15 -0700

    Initial Updates for Terminus 1.0 from Kyle Hall

--- README.md
+++ README.md
@@ -1,4 +1,49 @@
-# Pantheon Drupal Auto Update
-A little bit of magic for automatically updating Drupal sites. 
+# Pantheon Drupal Auto Update #
 
-This is based on Andrew Taylor's work for WordPress available at https://github.com/ataylorme/wordpress-at-scale-auto-update.
+## Description ##
+Automate Drupal core, plugin and theme updates on [Pantheon](https://pantheon.io) with Terminus, CircleCI, Drush, BackstopJS and Slack.
+
+This script will:
+
+1. Authenticate with [Terminus](https://github.com/pantheon-systems/terminus) via machine token
+2. Delete the multidev environment `update-dr`
+3. Recreate the multidev environment `update-dr`
+	* Deletion and recreation is done to clear any existing changes and pull the latest database/files from the live environment 
+4. Switch the multidev environment `update-dr` to Git mode
+5. [Apply Pantheon upstream updates](https://pantheon.io/docs/upstream-updates/)
+	* Drupal core updates are managed in the upstream
+6. Switch the multidev environment `update-dr` to SFTP mode
+7. Check for and apply Drupal plugin updates via [Drush](http://www.drush.org), if available
+8. Check for and apply Drupal theme updates via [Drush](http://www.drush.org), if available
+	* If no Drupal updates are available the script will complete and report the Slack
+9. Use BackstopJS to run a visual regression test between the live environment and the multidev environment
+	* If discrepencies are found the script will fail and report the error to Slack
+10. Merge the multidev environment with the dev environment
+11. Deploy the dev environment to the test environment
+12. Deploy the test environment to the live environment
+13. Post a success message to Slack
+
+## License ##
+[GPLv2 or later](http://www.gnu.org/licenses/gpl-2.0.html)
+
+## Setup ##
+1. Create a [CircleCI](https://circleci.com) project
+2. Add [environment variables to CircleCI](https://circleci.com/docs/environment-variables/) for the following:
+	* `SITE_UUID`: The [Pantheon site UUID](https://pantheon.io/docs/sites/#site-uuid)
+	* `TERMINUS_MACHINE_TOKEN`: A [Pantheon Terminus machine token](https://pantheon.io/docs/machine-tokens/) with access to the site
+	* `SLACK_HOOK_URL`: The [Slack incoming webhook URL](https://api.slack.com/incoming-webhooks)
+	* `SLACK_CHANNEL`: The Slack channel to post notifications to
+	* `SLACK_USERNAME`: The username to post to Slack with
+3. Add an [SSH key to Pantheon](https://pantheon.io/docs/ssh-keys/) and [to the CircleCI project](https://circleci.com/docs/permissions-and-access-during-deployment/).
+4. Update the site UUID in the `.env` file
+5. Update _scenarios_ in `backstop.js` with URLs for pages you wish to check with visual regression
+	* `url` refers to the live URL and `referenceUrl` refers to the same page on the Pantheon multidev environment
+6. Ping the [CircleCI API](https://circleci.com/docs/api/) at the desired frequency, e.g. daily, to run the script
+
+## Notes ##
+This workflow assumes the `master` branch (dev) and test environments on Pantheon are always in a shippable state as the script will automatically deploy changes from dev to test and live.
+
+All incomplete work should be kept in a [Pantheon multidev environment](https://pantheon.io/docs/multidev/), on a separate Git branch.
+
+
+Inspired by [Andrew Taylor](https://github.com/ataylorme/) whose work on automatic updates for WordPress this project is based and [Kyle Hall](https://github.com/Ky1e) who brought joy and delight to Drupal utilizing Terminus 1.0. 
--- bin/drupal-auto-update.sh
+++ bin/drupal-auto-update.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-MULTIDEV="update-d"
+MULTIDEV="update-dr"
 
 UPDATES_APPLIED=false
 
 # login to Terminus
 echo -e "\nLogging into Terminus..."
-terminus auth login --machine-token=${TERMINUS_MACHINE_TOKEN}
+terminus auth:login --machine-token=${TERMINUS_MACHINE_TOKEN}
 
 # delete the multidev environment
 echo -e "\nDeleting the ${MULTIDEV} multidev environment..."
-terminus site delete-env --remove-branch --yes
+terminus multidev:delete $SITE_UUID.$MULTIDEV --delete-branch --yes
 
 # recreate the multidev environment
 echo -e "\nRe-creating the ${MULTIDEV} multidev environment..."
-terminus site create-env --from-env=live --to-env=${MULTIDEV}
+terminus multidev:create $SITE_UUID.live $MULTIDEV
 
 # check for upstream updates
 echo -e "\nChecking for upstream updates on the ${MULTIDEV} multidev..."
 # the output goes to stderr, not stdout
-UPSTREAM_UPDATES=$(terminus site upstream-updates list  --format=bash  2>&1)
+UPSTREAM_UPDATES="$(terminus upstream:updates:list $SITE_UUID.$MULTIDEV  --format=list  2>&1)"
 
 if [[ ${UPSTREAM_UPDATES} == *"No updates"* ]]
 then
@@ -28,42 +28,48 @@ then
 else
     # making sure the multidev is in git mode
     echo -e "\nSetting the ${MULTIDEV} multidev to git mode"
-    terminus site set-connection-mode --mode=git
+    terminus connection:set $SITE_UUID.$MULTIDEV git
 
     # apply Drupal upstream updates
     echo -e "\nApplying upstream updates on the ${MULTIDEV} multidev..."
-    terminus site upstream-updates apply --yes --updatedb --accept-upstream
+    terminus upstream:updates:apply $SITE_UUID.$MULTIDEV --yes --updatedb --accept-upstream
     UPDATES_APPLIED=true
 fi
 
 # making sure the multidev is in SFTP mode
 echo -e "\nSetting the ${MULTIDEV} multidev to SFTP mode"
-terminus site set-connection-mode --mode=sftp
+terminus connection:set $SITE_UUID.$MULTIDEV sftp
 
-# check for Drupal plugin updates
-echo -e "\nChecking for Drupal plugin updates on the ${MULTIDEV} multidev..."
-PLUGIN_UPDATES=$(terminus drush "pm-updatestatus" --format=bash)
+# check for Drupal module updates
+echo -e "\nChecking for Drupal module updates on the ${MULTIDEV} multidev..."
+PLUGIN_UPDATES="$(terminus drush $SITE_UUID.$MULTIDEV -- plugin list --update=available --format=count)"
 
-if [[ ${PLUGIN_UPDATES} != "" ]]
+if [[ ${PLUGIN_UPDATES} == "0" ]]
 then
-    # update Drupal plugins
-    echo -e "\nUpdating Drupal plugins on the ${MULTIDEV} multidev..."
-    terminus drush "pm-update --no-core -y"
-
-    # committing updated Drupal plugins
-    echo -e "\nCommitting Drupal plugin updates on the ${MULTIDEV} multidev..."
-    terminus site code commit --message="update Drupal plugins" --yes
-    UPDATES_APPLIED=true
+    # no Drupal module updates found
+    echo -e "\nNo Drupal module updates found on the ${MULTIDEV} multidev..."
 else
-    # no Drupal plugin updates found
-    echo -e "\nNo Drupal plugin updates found on the ${MULTIDEV} multidev..."
+    # update Drupal modules
+    echo -e "\nUpdating Drupal modules on the ${MULTIDEV} multidev..."
+    terminus drush $SITE_UUID.$MULTIDEV -- pm-update drupal
+
+    # wake the site environment before committing code
+    echo -e "\nWaking the ${MULTIDEV} multidev..."
+    terminus env:wake -n $SITE_UUID.$MULTIDEV
+
+    # committing updated Drupal modules
+    echo -e "\nCommitting Drupal modules updates on the ${MULTIDEV} multidev..."
+    terminus env:commit $SITE_UUID.$MULTIDEV --message="update Drupal modules" --yes
+    UPDATES_APPLIED=true
+   
+    
 fi
 
 if [[ "${UPDATES_APPLIED}" = false ]]
 then
     # no updates applied
     echo -e "\nNo updates to apply..."
-    SLACK_MESSAGE="Drupal Magic Update Site Circle CI update check #${CIRCLE_BUILD_NUM} by ${CIRCLE_PROJECT_USERNAME}. No updates to apply, nothing deployed."
+    SLACK_MESSAGE="No updates on build #${CIRCLE_BUILD_NUM} for ${CIRCLE_PROJECT_USERNAME}. I'm going back to sleep."
     echo -e "\nSending a message to the ${SLACK_CHANNEL} Slack channel"
     curl -X POST --data "payload={\"channel\": \"${SLACK_CHANNEL}\", \"username\": \"${SLACK_USERNAME}\", \"text\": \"${SLACK_MESSAGE}\"}" $SLACK_HOOK_URL
 else
@@ -75,7 +81,7 @@ else
 
     # ping the multidev environment to wake it from sleep
     echo -e "\nPinging the ${MULTIDEV} multidev environment to wake it from sleep..."
-    curl -I https://update-d-magic-drupal-updates.pantheonsite.io/
+    terminus env:wake -n $SITE_UUID.$MULTIDEV
 
     # backstop visual regression
     echo -e "\nRunning BackstopJS tests..."
@@ -94,7 +100,7 @@ else
     then
         # visual regression failed
         echo -e "\nVisual regression tests failed! Please manually check the ${MULTIDEV} multidev..."
-        SLACK_MESSAGE="Drupal Magic Update Site Circle CI update check #${CIRCLE_BUILD_NUM} by ${CIRCLE_PROJECT_USERNAME}. Visual regression tests failed on <https://dashboard.pantheon.io/sites/${SITE_UUID}#${MULTIDEV}/code|the ${MULTIDEV} environment>! Please test manually."
+        SLACK_MESSAGE="Circle CI update check #${CIRCLE_BUILD_NUM} by ${CIRCLE_PROJECT_USERNAME}. Visual regression tests failed on <https://dashboard.pantheon.io/sites/${SITE_UUID}#${MULTIDEV}/code|the ${MULTIDEV} environment>! Please test manually."
         echo -e "\nSending a message to the ${SLACK_CHANNEL} Slack channel"
         curl -X POST --data "payload={\"channel\": \"${SLACK_CHANNEL}\", \"username\": \"${SLACK_USERNAME}\", \"text\": \"${SLACK_MESSAGE}\"}" $SLACK_HOOK_URL
         exit 1
@@ -104,26 +110,30 @@ else
 
         # enable git mode on dev
         echo -e "\nEnabling git mode on the dev environment..."
-        terminus site set-connection-mode --env=dev --mode=git --yes
+        terminus connection:set $SITE_UUID.dev git
 
         # merge the multidev back to dev
         echo -e "\nMerging the ${MULTIDEV} multidev back into the dev environment (master)..."
-        terminus site merge-to-dev
-
+        terminus connection:set $SITE_UUID.dev git
+        
         # deploy to test
         echo -e "\nDeploying the updates from dev to test..."
-        terminus site deploy --env=test --sync-content --cc --note="Auto deploy of Drupal updates (core, plugin, themes)"
+        terminus env:deploy $SITE_UUID.test --sync-content --cc --note="Auto deploy of Drupal updates (core, modules)"
 
         # backup the live site
         echo -e "\nBacking up the live environment..."
-        terminus site backups create --env=live --element=all
+        terminus backup:create $SITE_UUID.live --element=all --keep-for=30
 
         # deploy to live
         echo -e "\nDeploying the updates from test to live..."
-        terminus site deploy --env=live --cc --note="Auto deploy of Drupal updates (core, plugin, themes)"
+        terminus env:deploy $SITE_UUID.live --cc --note="Auto deploy of Drupal updates (core, modules)"
+
+        # update Drupal database on live
+        echo -e "\nUpdating the database on the live environment..."
+        terminus drush $SITE_UUID.live -- pm-update drupal
 
         echo -e "\nVisual regression tests passed! Drupal updates deployed to live..."
-        SLACK_MESSAGE="Drupal Magic Update Site Circle CI update check #${CIRCLE_BUILD_NUM} by ${CIRCLE_PROJECT_USERNAME} Visual regression tests passed! Drupal updates deployed to <https://dashboard.pantheon.io/sites/${SITE_UUID}#live/deploys|the live environment>."
+        SLACK_MESSAGE="I've updated ${CIRCLE_PROJECT_REPONAME} on build #${CIRCLE_BUILD_NUM} and the visual regression tests passed! Drupal updates deployed to <https://dashboard.pantheon.io/sites/${SITE_UUID}#live/deploys|the live environment>."
         echo -e "\nSending a message to the ${SLACK_CHANNEL} Slack channel"
         curl -X POST --data "payload={\"channel\": \"${SLACK_CHANNEL}\", \"username\": \"${SLACK_USERNAME}\", \"text\": \"${SLACK_MESSAGE}\"}" $SLACK_HOOK_URL
     fi
