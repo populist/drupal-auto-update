3d0e69c3fb1382b8887260aa4ad295f43d7cdae8
Matt Cheney <matt@getpantheon.com>
2017-07-31 12:24:45 -0400

    Calling Drush PM-Update Status Twice

--- bin/drupal-auto-update.sh
+++ bin/drupal-auto-update.sh
@@ -46,7 +46,8 @@ terminus env:wake -n $SITE_UUID.$TERMINUS_ENV
 
 # check for Drupal module updates
 echo -e "\nChecking for Drupal module updates on the ${TERMINUS_ENV} multidev..."
-PLUGIN_UPDATES="$(terminus drush $SITE_UUID.$TERMINUS_ENV -- pm-updatestatus --format=list | grep -v OK)"
+terminus drush $SITE_UUID.$TERMINUS_ENV -- pm-updatestatus
+PLUGIN_UPDATES="$(terminus drush $SITE_UUID.$TERMINUS_ENV -- pm-updatestatus --format=list | grep -v ok)"
 
 if [[ ${PLUGIN_UPDATES} == "" ]]
 then
